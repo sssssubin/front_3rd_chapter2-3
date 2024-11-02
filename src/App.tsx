034f6311b7490cad9909a09ad2f4a1eef68db01d
유용태 <teo.v@navercorp.com>
2024-11-02 09:06:55 +0900

    tanstack-query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useAtomsDebugValue } from "jotai-devtools"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { queryClient } from "@/app/config/tanstack.query"
 import { BrowserRouter as Router } from "react-router-dom"
 import Footer from "./modules/ui/Footer.tsx"
 import Header from "./modules/ui/Header.tsx"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
 
 const App = () => {
-  useAtomsDebugValue()
-
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
